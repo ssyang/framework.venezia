743c7052ebd61d07e9333814eb6ec0271813bab1
seong-sig <ssyangsnsk@gmail.com>
2021-06-13 13:24:05 +0900

    _test_mul

--- test/t1/t1.cpp
+++ test/t1/t1.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
+#include <thread>         // std::this_thread::sleep_for
+#include <chrono>         // std::chrono::seconds
 
 #include <inc/c_variable.h>
 #include <inc/c_function.h>
 //#include <inc/function.h>
 
-class csqure : public _venezia::c_function
-{
-
-    protected:
-    _venezia::c_variable _forward(const _venezia::c_variable & data)
-    {
-        return data*data;
-    }
-};
 
 void _test_matrix()
 {
@@ -31,7 +24,7 @@ void _test_matrix()
 
     std::cout << " b = " <<std::endl;
     std::cout << b() <<std::endl;
-
+/*
     std::cout << (a*b)() <<std::endl;
 
     std::cout <<std::endl;
@@ -45,24 +38,110 @@ void _test_matrix()
 
     std::cout <<std::endl;
     std::cout << (1.2f*a).get() <<std::endl;
+*/
+    std::cout <<std::endl;
+
+    Eigen::MatrixXd data13(1,3);
+    data13 << 11,12,13;
+
+    Eigen::MatrixXd data31(3,1);
+    data31 << 31,32,33;
+
+    std::cout << data13 <<std::endl;
+    std::cout <<std::endl;
+
+    std::cout << data31 <<std::endl;
+    std::cout <<std::endl;
+
+    std::cout << data31 * data13 <<std::endl;
+    std::cout <<std::endl;
+
+    //
+    a = data13;
+    b = data31;
+    _venezia::c_variable c;
+
+    std::cout << (a*b)() <<std::endl;
+    std::cout <<std::endl;
+    std::cout << (b*a)() <<std::endl;
+    std::cout <<std::endl;
+
+}
+
+
+class csqure : public _venezia::c_function
+{
+
+    protected:
+    _venezia::c_variable _forward(const _venezia::c_variable & data)
+    {
+        //return data*data;
+        return data*data;
+    }
+};
+
+void _test_function()
+{
+    Eigen::MatrixXd data23(2,3);
+    data23 << 1,2,3,4,5,6;
+
+    _venezia::c_variable a(data23);
+    
+    csqure fun_squre;
+    _venezia::c_variable b = fun_squre(a);
+    if(b.error()){
+        std::cout <<" fun_squre : error. " <<std::endl;
+    }
+    else{
+        std::cout <<" b = " << std::endl;
+        std::cout << b() <<std::endl;
+    }
+}
+
+void _test_mul()
+{
+    _venezia::c_variable a,b,c;
+    size_t N(3),M(3);
+
+    for( size_t n_r = 0; n_r<N; n_r++ ){
+        for( size_t n_c = 0; n_c<M; n_c++ ){
+
+            Eigen::MatrixXd da(n_r,n_c);
+            da.fill(2);
+            a=da;
+
+            for( size_t i = 0; i<N; i++ ){
+                for( size_t j = 0; j<M; j++ ){
+                    Eigen::MatrixXd db(i,j);
+                    db.fill(3);
+                    b = db;
+                    c = a.mul(b);
+                    //
+                    if(c.error()){
+                        //std::cout << "error" <<std::endl;
+                    }
+                    else{
+                        std::cout << "(" << a.get().rows() << " X " << a.get().cols() << ")(" << b.get().rows() << " X " << b.get().cols() << ")"<<std::endl;
+                        std::cout << c.get() <<std::endl;
+                        //std::this_thread::sleep_for (std::chrono::seconds(2));                        
+                    }
+                }
+            }
+            std::cout <<std::endl;
+            
+        }//endf for
+    }//end for
 }
 
 int main()
 {
 
     std::cout << "test t1 gogo." <<std::endl;
-    _test_matrix();
-
+    //_test_matrix();
+    //_test_function();
+    _test_mul();
 
     //
-    Eigen::MatrixXd data23(2,3);
-    data23 << 11,12,13,21,22,23;
-
-    _venezia::c_variable a(data23);
-    
-    csqure fun_squre;
-    _venezia::c_variable b = fun_squre(a);
-    std::cout <<" b = "<< b() <<std::endl;
 
     /*
     _venezia::c_variable x(Eigen::MatrixXd(7));
--- venezia/inc/c_variable.h
+++ venezia/inc/c_variable.h
@@ -11,10 +11,15 @@ namespace _venezia
     class c_variable
     {
         public:
-        c_variable(){}
-        c_variable( const Eigen::MatrixXd & matrix_data )
+        c_variable() : m_b_last_error(false){}
+        c_variable( const Eigen::MatrixXd & matrix_data ) : m_b_last_error(false)
         {
             m_matrix_data = matrix_data;
+        }
+        c_variable( const c_variable& rhs )
+        {
+            m_matrix_data = rhs.get();
+            m_b_last_error = rhs.m_b_last_error;
         }
 
         ~c_variable(){}
@@ -23,7 +28,6 @@ namespace _venezia
         {
             m_matrix_data = matrix_data;
         }
-
         Eigen::MatrixXd get() const
         {
             return m_matrix_data;
@@ -34,9 +38,34 @@ namespace _venezia
             return m_matrix_data;
         }
 
+        bool error() const
+        {
+            return m_b_last_error;
+        }
+
+        /*
+        * matrix operators
+        */
+        c_variable& operator=(const c_variable& rhs)
+        {
+            m_matrix_data = rhs.get();
+            m_b_last_error = rhs.m_b_last_error;
+            return *this;
+        }
+        c_variable& operator=(const Eigen::MatrixXd& rhs)
+        {
+            m_matrix_data = rhs;
+            return *this;
+        }
+
         c_variable& operator+=(const c_variable& rhs)
         {
             m_matrix_data += rhs.get();
+            return *this;
+        }
+        c_variable& operator-=(const c_variable& rhs)
+        {
+            m_matrix_data -= rhs.get();
             return *this;
         }
 
@@ -45,22 +74,44 @@ namespace _venezia
             lhs += rhs;
             return lhs;
         }
-
-        c_variable& operator-=(const c_variable& rhs)
-        {
-            m_matrix_data -= rhs.get();
-            return *this;
-        }
-
         friend c_variable operator-( c_variable lhs, const c_variable& rhs )
         {
             lhs -= rhs;
             return lhs;
         }
 
+        /*
+        * multiply each item of matrix
+        */
         c_variable& operator*=(const c_variable& rhs)
         {
-            m_matrix_data *= rhs.get();
+            do{
+                size_t n_row(0),n_col(0);
+
+                if(m_matrix_data.rows() > rhs.get().rows()){
+                    n_row = rhs.get().rows();
+                }
+                else{
+                    n_row = m_matrix_data.rows();
+                }
+
+                if(m_matrix_data.cols() > rhs.get().cols()){
+                    n_col = rhs.get().cols();
+                }
+                else{
+                    n_col = m_matrix_data.cols();
+                }
+
+                Eigen::MatrixXd matrix_new(n_row,n_col);
+
+                for( size_t i=0; i<n_row; i++){
+                    for( size_t j=0; j<n_col; j++){
+                        matrix_new(i,j) = m_matrix_data(i,j) * rhs.get()(i,j);
+                    }
+                }//end for
+
+                m_matrix_data = matrix_new;
+            }while(false);
             return *this;
         }
 
@@ -138,7 +189,70 @@ namespace _venezia
             return rhs;
         }
 
+        // matirix multiplication
+        c_variable mul(const Eigen::MatrixXd &rhs)
+        {
+            c_variable result;
+            do{
+                result.m_b_last_error = true;
+                size_t n_row(0),n_col(0);
+
+                if(m_matrix_data.rows() == 0 ){
+                    continue;
+                }
+                if(m_matrix_data.cols() == 0 ){
+                    continue;
+                }
+                if(rhs.rows() == 0 ){
+                    continue;
+                }
+                if(rhs.cols() == 0 ){
+                    continue;
+                }
+
+                if(m_matrix_data.cols() == rhs.rows()){
+                    //(M1xN) * (NxM2) = (M1xM2)
+                    result = m_matrix_data*rhs;
+                    result.m_b_last_error = false;
+                    continue;
+                }
+
+                if(m_matrix_data.rows() == 1){
+                    if(rhs.rows() ==1 ){
+                        if(m_matrix_data.cols() == rhs.cols() ){
+                            //(1xN) * (1xN) -> (1xN) * (Nx1) = (1X1)
+                            result = m_matrix_data * rhs.transpose();
+                            result.m_b_last_error = false;
+                            continue;
+                        }
+                        //(1xN) * (1xM) , N != M
+                        continue;
+                    }
+                    //(1xN) * (MxN)
+                    continue;
+                }
+
+                if(rhs.rows() ==1 ){
+                    if(m_matrix_data.cols() == rhs.cols() ){
+                        //(MxN) * (1xN) -> (MxN) * (Nx1) = (Mx1)
+                        result = m_matrix_data * rhs.transpose();
+                        result.m_b_last_error = false;
+                        continue;
+                    }
+                    //(MxN1) * (1xN2)
+                    continue;
+                }
+                //(M1xN1) * (M2xN2)
+                continue;
+            }while(false);
+            return result;
+        }
+        c_variable mul(const c_variable &rhs)
+        {
+            return mul(rhs.get());
+        }
         private:
             Eigen::MatrixXd m_matrix_data;
+            bool m_b_last_error;
     };
 }
