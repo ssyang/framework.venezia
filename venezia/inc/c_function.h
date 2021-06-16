972183a5f376a9abb0677f70d7a8fe3478c01d79
seong-sig <ssyangsnsk@gmail.com>
2021-06-16 19:40:19 +0900

    test

--- venezia/inc/c_function.h
+++ venezia/inc/c_function.h
@@ -7,18 +7,31 @@
 namespace _venezia
 {
     /**
-     * @brief ths base class of all function 
+     * @brief interface
+     * Don't use this class in external
      * this is virtual class
      * 
      */
-    template <class T>
-    class c_function
+
+    class _c_function_
     {
         public:
-            typedef std::shared_ptr<c_function>     type_ptr_c_function;
-            typedef std::list<type_ptr_c_function>  type_list_ptr_c_function;
-            typedef std::shared_ptr<type_list_ptr_c_function> type_ptr_list_ptr_c_function;
+            typedef std::shared_ptr<_c_function_>     type_ptr_c_function_;
+            typedef std::list<type_ptr_c_function_>  type_list_ptr_c_function_;
+            typedef std::shared_ptr<type_list_ptr_c_function_> type_ptr_list_ptr_c_function_;
 
+        protected:
+            virtual _venezia::c_variable _default_forward(const _venezia::c_variable  & data) = 0;
+
+    };
+
+    /**
+     * @brief ths base class of all function 
+     * 
+     */
+    template <class T>
+    class c_function : public _c_function_
+    {
         public:
             c_function()
             {}
@@ -33,7 +46,7 @@ namespace _venezia
                 m_ptr_list_ptr_override_fun = f.m_ptr_list_ptr_override_fun;
                 return *this;
             }
-            c_function & operator=(const type_ptr_list_ptr_c_function & list_ptr_fun)
+            c_function & operator=(const _c_function_::type_ptr_list_ptr_c_function_ & list_ptr_fun)
             {
                 m_ptr_list_ptr_override_fun = list_ptr_fun;
                 return *this;
@@ -53,8 +66,10 @@ namespace _venezia
                     }
 
                     out_var = in_variable;
-                    std::for_each( m_ptr_list_ptr_override_fun->begin(),m_ptr_list_ptr_override_fun->end(),[&](type_ptr_c_function &ptr_fun){
-                        out_var = (*ptr_fun)(out_var);
+                    std::for_each( m_ptr_list_ptr_override_fun->begin(),m_ptr_list_ptr_override_fun->end(),[&](_c_function_::type_ptr_c_function_ &ptr_fun){
+                        if(ptr_fun){
+                            out_var = (*ptr_fun)(out_var);
+                        }
                     });
 
 
@@ -62,28 +77,33 @@ namespace _venezia
                 return  out_var;
             }
 
-            type_ptr_list_ptr_c_function operator()( const type_ptr_list_ptr_c_function & ptr_list_ptr_fun)
+            _c_function_::type_ptr_list_ptr_c_function_ operator()( const _c_function_::type_ptr_list_ptr_c_function_ & ptr_list_ptr_fun)
             {
-                type_ptr_list_ptr_c_function ptr_list_ptr_c_function=ptr_list_ptr_fun;
+                _c_function_::type_ptr_list_ptr_c_function_ ptr_list_ptr_c_function=ptr_list_ptr_fun;
 
                 if(!ptr_list_ptr_c_function){
-                    ptr_list_ptr_c_function = type_ptr_list_ptr_c_function( new type_list_ptr_c_function() );
+                    ptr_list_ptr_c_function = _c_function_::type_ptr_list_ptr_c_function_( new _c_function_::type_list_ptr_c_function_() );
                 }
-                type_ptr_c_function ptr_c_function( new T() );
+                _c_function_::type_ptr_c_function_ ptr_c_function( new T() );
                 ptr_list_ptr_c_function->push_back(ptr_c_function);
                 return ptr_list_ptr_c_function;
             }
-            type_ptr_list_ptr_c_function operator()()
+            _c_function_::type_ptr_list_ptr_c_function_ operator()()
             {
-                type_ptr_list_ptr_c_function ptr_list_ptr_c_function( new type_list_ptr_c_function() );
-                type_ptr_c_function ptr_c_function( new T() );
+                _c_function_::type_ptr_list_ptr_c_function_ ptr_list_ptr_c_function( new _c_function_::type_list_ptr_c_function_() );
+                _c_function_::type_ptr_c_function_ ptr_c_function( new T() );
                 ptr_list_ptr_c_function->push_back(ptr_c_function);
                 return ptr_list_ptr_c_function;
             }
 
         protected:
-            virtual _venezia::c_variable _default_forward(const _venezia::c_variable  & data) = 0;
+            virtual _venezia::c_variable _default_forward(const _venezia::c_variable  & data)
+            {
+                return data;
+            }
         protected:
-            type_ptr_list_ptr_c_function m_ptr_list_ptr_override_fun;
+            _c_function_::type_ptr_list_ptr_c_function_ m_ptr_list_ptr_override_fun;
     };
+
 }
+
